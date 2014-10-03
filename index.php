12d74977f0b08f40c007f14bc64f9af435314d93
Mile Janev <mile.janev89@yahoo.com>
2014-10-03 16:57:31 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+        <title>PHP Frameworks Speed Benchmark</title>
+        <link rel="stylesheet" type="text/css" href="style/main.css" />
+        <script src="js/main.js"></script>
+        <script src="js/jquery-1.11.1.min.js"></script>
+    </head>
+    <body>
+        test
+    </body>
+</html>
