3471f680d1df1c0ea1e1232b942740d5f04db801
mile <mile@Mile-Laptop>
2014-10-25 15:36:40 +0200

    Zend Project Early beginnings

--- projects/zend/application/Bootstrap.php
+++ projects/zend/application/Bootstrap.php
@@ -2,7 +2,18 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+   protected function _initDoctype()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('XHTML1_STRICT');
+    }
+    
+    protected function _initNamespaces()
+    {
+        Zend_Loader_Autoloader::getInstance()->registerNamespace("Frameworks_");        
+        Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(TRUE);
+    }
 
 }
 
