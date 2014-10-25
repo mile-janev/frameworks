c7a2c85dfbff1d5538c5c5a42fcb3fae5b351ea8
mile <mile@Mile-Laptop>
2014-10-25 19:58:01 +0200

    Zend Project All

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
 <div id="titleSite"><a href="<?php echo $serverName; ?>">PHP Frameworks Speed Benchmark</a></div>
 <ul id="mainMenu">
     <li<?php echo (preg_match('/yii/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/yii">Yii</a></li>
-    <li<?php echo (preg_match('/zend/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/zend">Zend</a></li>
+    <li<?php echo (preg_match('/zend/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/zend/public">Zend</a></li>
     <li<?php echo (preg_match('/codeigniter/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/codeigniter">Codeigniter</a></li>
     <li<?php echo (preg_match('/laravel/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/laravel">Laravel</a></li>
     <li<?php echo (preg_match('/symfony/',$currentUrl)) ? ' class="active"' : ''; ?>><a href="<?php echo $serverName; ?>/projects/symfony">Symfony</a></li>
---
+++ projects/zend/application/models/DbTable/SUser.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Class Application_Model_DbTable_SUser
+ */
+class Application_Model_DbTable_SUser extends Zend_Db_Table_Abstract
+{
+    /**
+     * @var string
+     */
+    protected $_name = 's_user';
+}
