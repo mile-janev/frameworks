3f774bf6a118d40862a51a393ccc4b378b2691b9
mile <mile@Mile-Laptop>
2014-10-27 22:39:14 +0100

    Global Project Menu fix

--- menu.php
+++ menu.php
@@ -29,22 +29,22 @@
     <li<?php echo (preg_match('/zend/',$currentUrl)) ? ' class="active"' : ''; ?>>
         <a href="<?php echo $serverName; ?>/projects/zend/public/">Zend</a>
             <ul class="subMenu">
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/statistic/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/statistic\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/statistic/">Statistic</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/select/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/select\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/select/">Select</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectall/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectall\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/selectall/">Select All</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectallparams/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectallparams\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/selectallparams/">Select All Params</a>
                 </li>
-<!--                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/update/',$currentUrl)) ? ' class="active"' : ''; ?>>
+<!--                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/update\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/update/">Update</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/delete/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/delete\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/zend/public/site/delete/">Delete</a>
                 </li>            -->
             </ul>
@@ -52,22 +52,22 @@
     <li<?php echo (preg_match('/codeigniter/',$currentUrl)) ? ' class="active"' : ''; ?>>
         <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php">Codeigniter</a>
         <ul class="subMenu">
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/statistic/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/statistic\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/statistic/">Statistic</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/select/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/select\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/select/">Select</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectall/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectall\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/selectall/">Select All</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectallparams/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/selectallparams\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/selectallparams/">Select All Params</a>
                 </li>
-<!--                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/update/',$currentUrl)) ? ' class="active"' : ''; ?>>
+<!--                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/update\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/update/">Update</a>
                 </li>
-                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/delete/',$currentUrl)) ? ' class="active"' : ''; ?>>
+                <li<?php echo (preg_match('/zend/',$currentUrl) && preg_match('/site\/delete\//',$currentUrl)) ? ' class="active"' : ''; ?>>
                     <a href="<?php echo $serverName; ?>/projects/codeigniter/index.php/site/delete/">Delete</a>
                 </li>-->
             </ul>
