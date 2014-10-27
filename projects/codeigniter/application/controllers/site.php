1a9fa56127d45e038bef44acc4ab6ffa65e604b5
mile <mile@Mile-Laptop>
2014-10-27 20:51:04 +0100

    Codeigniter Project MVC Structure view and controller

---
+++ projects/codeigniter/application/controllers/site.php
@@ -0,0 +1,67 @@
+<?php
+
+class Site extends CI_Controller
+{
+    private $s;
+    private $m;
+    private $l;
+    private $t;
+    private $userId;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->s = 0;
+        $this->m = 0;
+        $this->l = 0;
+        $this->t = 10;
+        $this->userId = 10000;
+    }
+    
+    public function index()
+    {
+        $this->load->view('site/index');
+    }
+    
+    public function statistic()
+    {
+        $this->load->view('site/statistic', array(
+            
+        ));
+    }
+    
+    /*Select one posts with condition*/
+    public function select()
+    {
+        $this->load->view('site/result', array(
+            'act' => 'Select',
+            'small' => $this->s,
+            'medium' => $this->m,
+            'large' => $this->l
+        ));
+    }
+    
+    /*Select all posts no parameters*/
+    public function selectall()
+    {
+        $this->load->view('site/result', array(
+            'act' => 'Select All',
+            'small' => $this->s,
+            'medium' => $this->m,
+            'large' => $this->l
+        ));
+    }
+    
+    /*Select all posts with parameters*/
+    public function selectallparams()
+    {
+        $this->load->view('site/result', array(
+            'act' => 'Select All Params',
+            'small' => $this->s,
+            'medium' => $this->m,
+            'large' => $this->l
+        ));
+    }
+    
+}
---
+++ projects/codeigniter/application/views/site/result.php
@@ -0,0 +1,6 @@
+<h1><?php echo $act; ?></h1>
+<?php
+    echo "S: " . $small . "<br />";
+    echo "M: " . $medium . "<br />";
+    echo "L: " . $large . "<br />";
+?>
