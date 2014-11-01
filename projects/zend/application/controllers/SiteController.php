a8c8d608ada085075fa0d3ba1a5cd35ba5e1112f
mile <mile@Mile-Laptop>
2014-11-01 19:15:17 +0100

    Zend Project Functions

--- projects/zend/application/controllers/SiteController.php
+++ projects/zend/application/controllers/SiteController.php
@@ -54,7 +54,6 @@ class SiteController extends Zend_Controller_Action
     {        
         // action body
     }
-//find(), fetchAll(), fetchRow(), 
     
     /*Test method fetchRow()*/
     public function fetchrowAction()
@@ -80,7 +79,7 @@ class SiteController extends Zend_Controller_Action
         $lPost = new Application_Model_DbTable_LPost();
         $small = microtime(true)+$this->t;
         while ($small >= microtime(true)) {
-            $mPost->fetchRow('id = ' . $this->i);
+            $lPost->fetchRow('id = ' . $this->i);
             $this->i++;
             $this->l++;
         }
@@ -95,7 +94,119 @@ class SiteController extends Zend_Controller_Action
         $this->render('result');
     }
     
-    /*Select one post with condition*/
+    /*Test method find(). Accept only primary key*/
+    public function findAction()
+    {
+        $sPost = new Application_Model_DbTable_SPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $sPost->find($this->i);
+            $this->i++;
+            $this->s++;
+        }
+        $this->i = 1;
+        
+        $mPost = new Application_Model_DbTable_MPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $mPost->find($this->i);
+            $this->i++;
+            $this->m++;
+        }
+        $this->i = 1;
+        
+        $lPost = new Application_Model_DbTable_LPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $lPost->find($this->i);
+            $this->i++;
+            $this->l++;
+        }
+        $this->i = 1;
+        
+        $this->saveTest("find()");
+        
+        $this->view->assign('act', 'find()');
+        $this->view->assign('small', $this->s);
+        $this->view->assign('medium', $this->m);
+        $this->view->assign('large', $this->l);
+        $this->render('result');
+    }
+    
+    /*Test method fetchAll()*/
+    public function fetchallAction()
+    {
+        $sPost = new Application_Model_DbTable_SPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $sPost->fetchAll('id = ' . $this->i);
+            $this->i++;
+            $this->s++;
+        }
+        $this->i = 1;
+        
+        $mPost = new Application_Model_DbTable_MPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $mPost->fetchAll('id = ' . $this->i);
+            $this->i++;
+            $this->m++;
+        }
+        $this->i = 1;
+        
+        $lPost = new Application_Model_DbTable_LPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $lPost->fetchAll('id = ' . $this->i);
+            $this->i++;
+            $this->l++;
+        }
+        $this->i = 1;
+        
+        $this->saveTest("fetchAll()");
+        
+        $this->view->assign('act', 'fetchAll()');
+        $this->view->assign('small', $this->s);
+        $this->view->assign('medium', $this->m);
+        $this->view->assign('large', $this->l);
+        $this->render('result');
+        
+    }
+    
+    /*Select all posts no parameters*/
+    public function selectallAction()
+    {
+        $sPost = new Application_Model_DbTable_SPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $sPost->fetchAll();
+            $this->s++;
+        }
+        
+        $mPost = new Application_Model_DbTable_MPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $mPost->fetchAll();
+            $this->m++;
+        }
+        
+        $lPost = new Application_Model_DbTable_LPost();
+        $small = microtime(true)+$this->t;
+        while ($small >= microtime(true)) {
+            $lPost->fetchAll();
+            $this->l++;
+        }
+        
+        $this->saveTest("selectall");
+        
+        $this->view->assign('act', 'selectall');
+        $this->view->assign('small', $this->s);
+        $this->view->assign('medium', $this->m);
+        $this->view->assign('large', $this->l);
+        $this->render('result');
+    }
+    
+    /*Test method fetch(). Only on query object (custom query). Same as fetchRow()*/
     public function fetchAction()
     {
         $select1 = new Zend_Db_Select($this->db);
@@ -131,126 +242,6 @@ class SiteController extends Zend_Controller_Action
         $this->saveTest("fetch()");
         
         $this->view->assign('act', 'fetch()');
-        $this->view->assign('small', $this->s);
-        $this->view->assign('medium', $this->m);
-        $this->view->assign('large', $this->l);
-        $this->render('result');
-    }
-    
-    /*Select one post with condition*/
-    public function selectAction()
-    {
-        $select1 = new Zend_Db_Select($this->db);
-        $select1 = $this->db->select()->from('s_post');
-        $small = microtime(true)+$this->t;
-        while ($small >= microtime(true)) {
-            $select1->where('id = ?', $this->userId);
-            $select1->query()->fetch();
-            $this->userId++;
-            $this->s++;
-        }
-        
-        $select2 = new Zend_Db_Select($this->db);
-        $select2 = $this->db->select()->from('m_post');
-        $medium = microtime(true)+$this->t;
-        while ($medium >= microtime(true)) {
-            $select2->where('id = ?', $this->userId);
-            $select2->query()->fetch();
-            $this->userId++;
-            $this->m++;
-        }
-        
-        $select3 = new Zend_Db_Select($this->db);
-        $select3 = $this->db->select()->from('l_post');
-        $large = microtime(true)+$this->t;
-        while ($large >= microtime(true)) {
-            $select3->where('id = ?', $this->userId);
-            $select3->query()->fetch();
-            $this->userId++;
-            $this->l++;
-        }
-        
-        $this->saveTest("select");
-        
-        $this->view->assign('act', 'Select');
-        $this->view->assign('small', $this->s);
-        $this->view->assign('medium', $this->m);
-        $this->view->assign('large', $this->l);
-        $this->render('result');
-    }
-    
-    /*Select all posts no parameters*/
-    public function selectallAction()
-    {        
-        $select1 = new Zend_Db_Select($this->db);
-        $select1 = $this->db->select()->from('s_post');
-        $small = microtime(true)+$this->t;
-        while ($small >= microtime(true)) {
-            $select1->query()->fetchAll();
-            $this->s++;
-        }
-        
-        $select2 = new Zend_Db_Select($this->db);
-        $select2 = $this->db->select()->from('m_post');
-        $medium = microtime(true)+$this->t;
-        while ($medium >= microtime(true)) {
-            $select2->query()->fetchAll();
-            $this->m++;
-        }
-        
-        $select3 = new Zend_Db_Select($this->db);
-        $select3 = $this->db->select()->from('l_post');
-        $large = microtime(true)+$this->t;
-        while ($large >= microtime(true)) {
-            $select3->query()->fetchAll();
-            $this->l++;
-        }
-        
-        $this->saveTest("selectall");
-        
-        $this->view->assign('act', 'Select All');
-        $this->view->assign('small', $this->s);
-        $this->view->assign('medium', $this->m);
-        $this->view->assign('large', $this->l);
-        $this->render('result');
-    }
-    
-    /*Select all posts no parameters*/
-    public function selectallparamsAction()
-    {
-        $select1 = new Zend_Db_Select($this->db);
-        $select1 = $this->db->select()->from('s_post');
-        $small = microtime(true)+$this->t;
-        while ($small >= microtime(true)) {
-            $select1->where('id = ?', $this->userId);
-            $select1->query()->fetchAll();
-            $this->userId++;
-            $this->s++;
-        }
-        
-        $select2 = new Zend_Db_Select($this->db);
-        $select2 = $this->db->select()->from('m_post');
-        $medium = microtime(true)+$this->t;
-        while ($medium >= microtime(true)) {
-            $select2->where('id = ?', $this->userId);
-            $select2->query()->fetchAll();
-            $this->userId++;
-            $this->m++;
-        }
-        
-        $select3 = new Zend_Db_Select($this->db);
-        $select3 = $this->db->select()->from('l_post');
-        $large = microtime(true)+$this->t;
-        while ($large >= microtime(true)) {
-            $select3->where('id = ?', $this->userId);
-            $select3->query()->fetchAll();
-            $this->userId++;
-            $this->l++;
-        }
-        
-        $this->saveTest("selectallparams");
-        
-        $this->view->assign('act', 'Select All Params');
         $this->view->assign('small', $this->s);
         $this->view->assign('medium', $this->m);
         $this->view->assign('large', $this->l);
