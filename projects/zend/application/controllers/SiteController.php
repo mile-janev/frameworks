163c0e5f032af043cd9893c99c753a9fb6837d1b
Mile Janev <mile.janev89@yahoo.com>
2014-11-05 16:46:03 +0100

    Library class used

--- projects/zend/application/controllers/SiteController.php
+++ projects/zend/application/controllers/SiteController.php
@@ -248,60 +248,17 @@ class SiteController extends Zend_Controller_Action
     
     public function indexAction()
     {        
-        /*find() start*/
-        $statistic = new Application_Model_DbTable_Statistic();
-        $findResults = $statistic->fetchAll("framework = 'zend' AND function = 'find()'")->toArray();
-        $findData = $this->formatArray($findResults);
-        /*find() end*/
+        include_once $_SERVER['DOCUMENT_ROOT'].'/Library.php';
+        $object = new Library();
         
-        /*fetchRow() start*/
-        $statistic = new Application_Model_DbTable_Statistic();
-        $fetchRowResults = $statistic->fetchAll("framework = 'zend' AND function = 'fetchRow()'")->toArray();
-        $fetchRowData = $this->formatArray($fetchRowResults);
-        /*fetchRow() end*/
+        /*find()*/
+        $findData = $object->findStatistic('zend', 'find()');
+        
+        /*fetchRow()*/
+        $fetchRowData = $object->findStatistic('zend', 'fetchRow()');
         
         $this->view->assign('findData', $findData);
         $this->view->assign('fetchRowData', $fetchRowData);        
     }
-    
-    /*Prepare results for displaying in chart*/
-    public function formatArray($statistic)
-    {
-        $results = $this->cleanResults($statistic);
-        $formatedArray = array();
-        $small = 0;
-        $medium = 0;
-        $large = 0;
-        $time = 0;
-        
-        foreach ($results as $result) {
-            $small += $result['small'];
-            $medium += $result['medium'];
-            $large += $result['large'];
-            $time += $result['execution_time'];
-        }
-        
-        $interval = 60/$time;//Ova e interval, posle go mnozeme so small/medium/large za 
-                            //da dobieme kolku e prosekot na izvrseni upiti vo minuta
-        
-        $formatedArray['tests'] = count($results);
-        $formatedArray['small'] = $small*$interval;
-        $formatedArray['medium'] = $medium*$interval;
-        $formatedArray['large'] = $large*$interval;
-        
-        return $formatedArray;
-    }
-    
-    //This function remove 5% of highest and 5% of lowest results for each database type (small/medium/large)
-    //Means that 70% of the tests will be accepted.
-    //
-    //Ova se pravi bidejki tie 30% se ne tolku realni poradi faktot sto procesorot vo
-    //toj moment mozel da bide zafaten pa pokazal pomal broj na zapisi ili drug nekoj faktor.
-    public function cleanResults($results)
-    {
-        
-        return $results;
-    }
-
 }
 
