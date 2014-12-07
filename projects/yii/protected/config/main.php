ab214063a39c940bd3ee3026a06207ea9d686e94
Mile Janev <Mile Janev@Mj-laptop>
2014-12-07 23:43:14 +0100

    Global Project

--- projects/yii/protected/config/main.php
+++ projects/yii/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'PHP Frameworks Performance Benchmark',
+	'name'=>'PHP Frameworks Performance Benchmark with Database Operations',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -56,7 +56,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=frameworks',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'toor',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
